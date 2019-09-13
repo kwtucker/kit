bc3ad8b952623edd86b3339fd43ab0a76cff10e9
kevin_tucker2 <kevin_tucker2@comcast.com>
2019-09-13 11:01:48 -0600

    concurrent request

--- sctevs-ids-delete/main.go
+++ sctevs-ids-delete/main.go
@@ -4,11 +4,18 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"io"
+	"log"
 	"net/http"
 	"os"
+	"runtime"
 	"strings"
+	"time"
 )
+
+type Response struct {
+	*http.Response
+	err error
+}
 
 const (
 	// Prod
@@ -18,13 +25,19 @@ const (
 )
 
 var (
-	Account string
-	Token   string
+	concurrentMax int
+	Account       string
+	Token         string
 )
 
-func main() {
+func init() {
+	flag.IntVar(&concurrentMax, "concurrent", 200, "Maximum concurrent requests")
 	flag.StringVar(&Account, "account", "", "Account number")
 	flag.StringVar(&Token, "token", "", "MPX Token")
+}
+
+func main() {
+
 	flag.Parse()
 
 	if Token == "" || Account == "" {
@@ -32,40 +45,99 @@ func main() {
 		return
 	}
 
-	list := []string{}
+	urls := []string{}
+
 	// Open the file.
 	f, _ := os.Open("ids.txt")
+
 	// Create a new Scanner for the file.
 	scanner := bufio.NewScanner(f)
+
 	// Loop over all lines in the file and print them.
 	for scanner.Scan() {
 		line := scanner.Text()
-		list = append(list, strings.TrimSpace(line))
+		if strings.TrimSpace(line) == "" {
+			continue
+		}
+		url := sctevsRaw + "/" + Account + "/" + strings.TrimSpace(line) + "?token=" + Token
+		urls = append(urls, url)
 	}
 
-	for _, id := range list {
-		url := sctevsRaw + "/" + Account + "/" + id + "?token=" + Token
-		fmt.Println(url)
-		request(url)
+	if len(urls) == 0 {
+		os.Exit(1)
+	}
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	reqChan := make(chan *http.Request)
+	respChan := make(chan Response)
+
+	start := time.Now()
+	go dispatcher(urls, reqChan)
+	go workerPool(reqChan, respChan)
+	conns, size := consumer(len(urls), respChan)
+	took := time.Since(start)
+	ns := took.Nanoseconds()
+	av := ns / conns
+	average, err := time.ParseDuration(fmt.Sprintf("%d", av) + "ns")
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Printf("Connections:\t%d\nConcurrent:\t%d\nTotal size:\t%d bytes\nTotal time:\t%s\nAverage time:\t%s\n", conns, concurrentMax, size, took, average)
+
+}
+
+// Dispatcher
+func dispatcher(urls []string, reqChan chan *http.Request) {
+	defer close(reqChan)
+	for _, url := range urls {
+		req, err := http.NewRequest(http.MethodDelete, url, nil)
+		if err != nil {
+			log.Println(err)
+		}
+		reqChan <- req
 	}
 }
 
-func request(url string) error {
-	var httpClient http.Client
-	var request *http.Request
-	var contentBuffer io.Reader
-	var err error
-
-	request, err = http.NewRequest(http.MethodDelete, url, contentBuffer)
-	if err != nil {
-		errorMsg := fmt.Sprintf("Unable to create request: %v", err)
-		fmt.Println(errorMsg)
+// Worker Pool
+func workerPool(reqChan chan *http.Request, respChan chan Response) {
+	t := &http.Transport{}
+	for i := 0; i < concurrentMax; i++ {
+		go worker(t, reqChan, respChan)
 	}
+}
 
-	_, err = httpClient.Do(request)
-	if err != nil {
-		fmt.Println(err)
+// Worker
+func worker(t *http.Transport, reqChan chan *http.Request, respChan chan Response) {
+	for req := range reqChan {
+		resp, err := t.RoundTrip(req)
+		r := Response{resp, err}
+		respChan <- r
 	}
+}
 
-	return err
+// Consumer
+func consumer(urlCount int, respChan chan Response) (int64, int64) {
+	var (
+		conns int64
+		size  int64
+	)
+
+	for conns < int64(urlCount) {
+		select {
+		case r, ok := <-respChan:
+			if ok {
+				if r.err != nil {
+					log.Println(r.err)
+				} else {
+					size += r.ContentLength
+					if err := r.Body.Close(); err != nil {
+						log.Println(r.err)
+					}
+				}
+				conns++
+			}
+		}
+	}
+	return conns, size
 }
